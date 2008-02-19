7d381952947234be3cc73405747e173b04502532
Marcus Epperson <mrepper@gmail.com>
2008-02-19 23:43:53 +0000

    * update meminfo function to work correctly with a 2.6 kernel   whether or not this function is still useful in general is another matter

--- cbench/opensource/HA4/meminfo.c
+++ cbench/opensource/HA4/meminfo.c
@@ -25,10 +25,60 @@
 #include <stdio.h>
 #include <string.h>
 
+//#define Dprintf printf
+#define Dprintf
+
+/* Returns total memory in kB in first param and swap used in kB in second param.
+ * 2.6 kernel version -mre
+ */
+int meminfo(unsigned long long *mem, unsigned long long *swap_used) {
+        FILE *meminfo;
+        char label[100];
+        unsigned long long data1;
+        unsigned long long swaptotal, swapfree;
+        int mem_found = 0;
+        int swap_found = 0;
+
+        if ( ( meminfo = fopen("/proc/meminfo", "r") ) == NULL ) {
+                perror("fopen");
+                return -1;
+        }
+
+        while (
+                !(mem_found && (swap_found > 1)) &&
+                (fscanf(meminfo, "%s %llu kB", label, &data1) >= 1)
+              ) {
+                if ( ! strncmp(label, "MemTotal:", 9) ) {
+                        Dprintf("DEBUG: mem found  (%s %llu kB)\n", label, data1);
+                        *mem = data1;
+                        mem_found++;
+                } else if ( ! strncmp(label, "SwapTotal:", 10) ) {
+                        Dprintf("DEBUG: swap found (%s %llu kB)\n", label, data1);
+                        swaptotal = data1;
+                        swap_found++;
+                } else if ( ! strncmp(label, "SwapFree:", 9) ) {
+                        Dprintf("DEBUG: swap found (%s %llu kB)\n", label, data1);
+                        swapfree = data1;
+                        swap_found++;
+                }
+        }
+
+        if (swap_found > 1)
+            *swap_used = swaptotal - swapfree;
+
+        fclose(meminfo);
+        Dprintf("DEBUG: mem_found = %d\tswap_found = %d\n", mem_found, swap_found);
+
+        if ( !mem_found || (swap_found < 2) )
+            return -2;
+
+        return 0;
+}
+
 /* Returns total memory in kB in first param and swap used in kB in second param.
  * For 2.4 kernels only (meminfo changed format in 2.5)
  */
-int meminfo(unsigned long long *mem, unsigned long long *swap_used) {
+int meminfo_OLD(unsigned long long *mem, unsigned long long *swap_used) {
         FILE *meminfo;
         char label[100];
         unsigned long long data1, data2;
@@ -45,18 +95,18 @@ int meminfo(unsigned long long *mem, unsigned long long *swap_used) {
                 (fscanf(meminfo, "%5s %llu %llu", label, &data1, &data2) >= 1)
               ) {
                 if ( ! strncmp(label, "Mem:", 4) ) {
-                        //printf("DEBUG: mem found  (%s %llu %llu)\n", label, data1, data2);
+                        Dprintf("DEBUG: mem found  (%s %llu %llu)\n", label, data1, data2);
                         *mem = data1 >> 10;
                         mem_found++;
                 } else if ( ! strncmp(label, "Swap:", 5) ) {
-                        //printf("DEBUG: swap found (%s %llu %llu)\n", label, data1, data2);
+                        Dprintf("DEBUG: swap found (%s %llu %llu)\n", label, data1, data2);
                         *swap_used = data2 >> 10;
                         swap_found++;
                 }
         }
 
         fclose(meminfo);
-        //printf("DEBUG: mem_found = %d\tswap_found = %d\n", mem_found, swap_found);
+        Dprintf("DEBUG: mem_found = %d\tswap_found = %d\n", mem_found, swap_found);
 
         if ( !mem_found || !swap_found )
             return -2;
