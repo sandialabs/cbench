728809da61d011289d1f9cb59c6a61b3ec24cd28
Jeffry Ogden <jbogden@sandia.gov>
2009-02-18 23:55:36 +0000

    'com' output parse module updated to deal with the com version found in the Phloem 1.0.0 benchmark
    updated the com job template in the bandwidth testset
    for the newer com version usage
    
    added a com job template in the latency testset
    for the newer com version which can do latency as well

--- perllib/output_parse/com.pm
+++ perllib/output_parse/com.pm
@@ -34,7 +34,7 @@ my ($shortpackage) = $package =~ /output_parse::(\S+)/;
 com
 
 Cbench parse module responsible for parsing output files generated
-by the Presta 'com' benchmark
+by the LLNL Presta 'com' benchmark
 .
 
 =cut
@@ -45,16 +45,6 @@ by the Presta 'com' benchmark
 #
 
 
-=head1 PUBLIC METHODS
-
-=over 4
-
-=item B<new()> - Create a new parse object
-
-  $obj = output_parse::com->new();
-
-=cut
-
 sub new {
 	my $type = shift;
 	my $self = {};
@@ -64,27 +54,6 @@ sub new {
 	return $self;
 }
 
-
-=item B<parse()> - Responsible for actually parsing the output
-contained in an output file and returning a hash reference with
-the extracted data.
-
-parse() takes a reference to a buffer (array) as the single input
-
-  $obj = output_parse::com->new();
-  .
-  read in an output file into @buf
-  .
-  $datahashref = $obj->parse(\@buf);
-  for $k (keys %{$datahashref}) {
-  print "$k => $datahashref->{$k}\n";
-  }
-
-The implementation of the parse() routine/method for a parse
-module encompasses most of the work in creating a new
-parse module.
-
-=cut
 
 sub parse {
 	my $self = shift;
@@ -98,26 +67,87 @@ sub parse {
 
 	my %data;
 
+	# we actually handle two generations of com versions now. the newer versions 
+	# have more variable output making the parsing more complicated
+	my $comversion = 'original';
+
 	# process all the lines in the STDOUT buffer
 	my $txtbuf = $bufrefs[0];
 	my $numlines = scalar @$txtbuf; 
 
     my $status = 'NOTSTARTED';
 	my $found_endrecord = 0;
+	my $table = '';
+	my $sawsummary = 0;
+
     foreach my $l (@{$txtbuf}) {
         ($l =~ /CBENCH NOTICE/) and $status = $l;
-        ($l =~ /Unidir.*Bidir.*Comm.*/) and $status = 'STARTED';
-		($l =~ /Max Unidirectional/) and $found_endrecord = 1 and
-			$status = 'COMPLETED';
-		$found_endrecord or next;
-		
-    	if ($l =~ /Max Unidirectional.*\:\s+(\d+\.\d+)\s+for.*/) {
-			$data{'unidir_bw'} = $1;
-    	}
-    	if ($l =~ /Max  Bidirectional.*\:\s+(\d+\.\d+)\s+for.*/) {
-			$data{'bidir_bw'} = $1;
-    	}
+        if ($l =~ /Unidirectional and Bidirectional Communication Test/) {
+			# original com version output looks like
+			$status = 'STARTED';
+		}
+        elsif ($l =~ /com Point-to-Point MPI Bandwidth and Latency Benchmark/) {
+			# newer com version output looks like
+			$comversion = 'new';
+			$status = 'STARTED';
+			main::debug_print(2,"DEBUG:$shortpackage.parse() newer com version");
+		}
 
+		if ($comversion =~ 'orig') {
+			($l =~ /Max Unidirectional/) and $found_endrecord = 1 and
+				$status = 'COMPLETED';
+			$found_endrecord or next;
+			
+			if ($l =~ /Max Unidirectional.*\:\s+(\d+\.\d+)\s+for.*/) {
+				$data{'unidir_bw'} = $1;
+			}
+			if ($l =~ /Max  Bidirectional.*\:\s+(\d+\.\d+)\s+for.*/) {
+				$data{'bidir_bw'} = $1;
+			}
+		}
+		elsif ($comversion =~ 'new') {
+			# Unidirectional Test Results
+			# (tasks, size, ops/sample, samples) : min/mean/max
+			# --------------------------------------------------------------------------------
+			# (     2,      32,   100,     1):         4.024 /        4.024 /        4.024
+			# ...
+			# Summary  :         min/mean/max =       41.442 /     1977.469 /     6352.254
+
+			# new test result table
+			if ($l =~ /(\S+) Test Results/) {
+				$table = $1;
+				$sawsummary = 0;
+				main::debug_print(2,"DEBUG:$shortpackage.parse() result table $table");
+			}
+
+			# Summary data line
+			if ($l =~ /Summary\s+:\s+\S+\s+=\s+(\S+)\s+\/\s+(\S+)\s+\/\s+(\S+)\s+/) {
+				$sawsummary = 1;
+				main::debug_print(2,"DEBUG:$shortpackage.parse() table $table summary: $1 $2 $3");
+				$data{"min_$table"} = $1;
+				
+				# if this is a NOT a latency result table record the mean/max
+				if ($table !~ /Latency/) {
+					$data{"ave_$table"} = $2;
+					$data{"max_$table"} = $3;
+				}
+			}
+
+			# catch the 8 byte latency for the job numprocs
+			if ($table =~ /Latency/ and
+				$l =~ /\(\s+$main::np,\s+8,\s+\S+,\s+\S+\):\s+(\S+)\s+\/\s+(\S+)\s+\/\s+(\S+)\s+/) {
+				main::debug_print(2,"DEBUG:$shortpackage.parse() table $table 8byte @ $main::np procs is $1");
+				$data{"8byte_$table"} = $1;
+			}
+
+			# a Summary line followed closely by a ######### line means the
+			# test ended normally
+			if ($l =~ /################################/ and $sawsummary) {
+				$found_endrecord = 1;
+				$status = 'COMPLETED';
+				main::debug_print(2,"DEBUG:$shortpackage.parse() com exited normally");
+			}
+		}
 	}
 
 	if ($status =~ /COMPLETED/) {
@@ -139,27 +169,10 @@ sub parse {
 	return \%data;
 }
 
-=item B<name()> - Return the name of the parse module
-
-For example: output_parse::xhpl
-
-=cut
-
 sub name {
 	my $self = shift;
 	return $self->{NAME};
 }
-
-=item B<files_list()> - Return the ordered array of file(s) we
-want to parse
-
-For example: ['STDOUT','STDERR'] or  ['STDOUT','hpccoutf.txt']
-
-STDOUT and STDERR are keywords and not actual filenames. The
-keywords are groked by the output parsing core code and handled
-appropriately.
-
-=cut
 
 sub file_list {
 	my $self = shift;
@@ -167,12 +180,6 @@ sub file_list {
 	return \@{$self->{FILE_LIST}};
 }
 
-
-=item B<metric_units()> - Return a hash with a mapping of the
-metrics this module returns to a string specifying the units
-of the metric
-
-=cut
 
 sub metric_units {
 	my $self = shift;
@@ -185,17 +192,6 @@ sub metric_units {
 #
 # "Private" methods
 #
-
-=head1 PRIVATE METHODS
-
-=over 4
-
-=item B<_init> - parse arguments for B<new()>
-
-Parse the arguments passed to B<new()> and sets the
-appropriate object variables and such.
-
-=cut
 
 sub _init {
 	my $self = shift;
@@ -213,6 +209,20 @@ sub _init {
 	%{$self->{METRIC_UNITS}} = (
 		'unidir_bw' => 'MB/s',
 		'bidir_bw' => 'MB/s',
+		'ave_UnidirAsync' => 'MB/s',
+		'min_BidirAsync' => 'MB/s',
+		'max_BidirAsync' => 'MB/s',
+		'min_UnidirAsync' => 'MB/s',
+		'ave_BidirAsync' => 'MB/s',
+		'max_UnidirAsync' => 'MB/s',
+		'max_Unidirectional' => 'MB/s',
+		'max_Bidirectional' => 'MB/s',
+		'ave_Unidirectional' => 'MB/s',
+		'min_Unidirectional' => 'MB/s',
+		'min_Bidirectional' => 'MB/s',
+		'ave_Bidirectional' => 'MB/s',
+		'min_Latency' => 'us',
+		'8byte_Latency' => 'us',
 	);
 	
 	return 1;
