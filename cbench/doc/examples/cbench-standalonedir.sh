f10768a96450642cde5bc0d73e3a9e0ed5480a0d
Chris Maestas <cdm4hp@4d497cd4-9c37-0410-8cfb-aadf10a5318b>
2011-03-19 14:06:59 +0000

    CDM: update to standalone dir example

--- cbench/doc/examples/cbench-standalonedir.sh
+++ cbench/doc/examples/cbench-standalonedir.sh
@@ -1,23 +1,20 @@
 # the required variables
 export CBENCHOME=$HOME/cbench
-export CBENCHSTANDALONEDIR=/tmp/cbench-test
-# add the Cbench tools directory to the path to pickup
-# cbench commands more easily
-export PATH=$PATH:$CBENCHOME/tools
-# a favorite alias for seeing what Cbench env looks like
-alias showcb='env | grep --color=always -e CBENCH -e MPIHOME -e COMPILER -e RPATH -e BLASLIB -e FFTW -e LAPACK'
+export CBENCHSTANDALONEDIR=$HOME/cbench-test
 
 #
 # needed if you want to build mpi w/ a different compiler
 #
 #export COMPILERCOLLECTION=gcc
 
-### RECOMMENDED OPTIONS
-
-# BLAS LIBRARY
+export LD_LIBRARY_PATH=$CBENCHSTANDALONEDIR/GotoBLAS2:$LD_LIBRARY_PATH
 export BLASLIB="-L$CBENCHSTANDALONEDIR/GotoBLAS2 -lgoto2 -lgfortran"
+export LAPACKLIB="-L$CBENCHSTANDALONEDIR/GotoBLAS2 -lgoto2"
 
-#### probably need to add in lapack and fftw variables here too
-# some other environment variables we honor
-#export LAPACKLIB="-L/path/to -lLAPACK"
+# add the Cbench tools directory to the path to pickup
+# cbench commands more easily
+export PATH=$PATH:$CBENCHOME/tools
+
+# a favorite alias for seeing what Cbench env looks like
+alias showcb='env | grep --color=always -e CBENCH -e MPIHOME -e COMPILER -e RPATH -e BLASLIB -e FFTW -e LAPACK'
 
